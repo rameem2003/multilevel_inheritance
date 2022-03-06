44d148c8a92537b851d96468431aea3b2fde78b0
rameem2003 <rameem2019@gmail.com>
2022-03-06 21:32:11 +0600

    Add project files.

---
+++ multilevel_inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace multilevel_inheritance
+{
+    class grand_father
+    {
+        public string grand_fa_name = "Abul Hossain.";
+    }
+
+    class father: grand_father
+    {
+        public string father_name = "A N M Mudassir Hossain.";
+    }
+
+
+    class me : father
+    {
+        public string my_name = "Mahmood Hassan Rameem.";
+    }
+
+
+    class app
+    {
+        public static void Main()
+        {
+            me obj = new me();
+
+            Console.WriteLine("Grand father name is: " + obj.grand_fa_name);
+            Console.WriteLine("Father name is: " + obj.father_name);
+            Console.WriteLine("My name is: " + obj.my_name);
+        }
+    }
+}
